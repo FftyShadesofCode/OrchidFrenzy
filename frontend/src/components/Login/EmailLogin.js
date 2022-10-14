02a94a26a00df83361c30643fca49741a11f2448
Ryan Howard <ryanghoward@protonmail.com>
2022-10-14 15:49:32 -0700

    Building Homepage

--- frontend/src/components/Login/EmailLogin.js
+++ frontend/src/components/Login/EmailLogin.js
@@ -1,40 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const EmailLogin = () => {
-    return (
-        <div className={`bg-cTenebrosa bg-cover bg-center w-screen h-screen text-primaryCatt`}>
-            <div className={`bg-black/80 w-full h-screen `}>
+  return (
+    <div
+      className={`bg-cTenebrosa bg-cover bg-center w-screen h-screen text-primaryCatt`}
+    >
+      <div className={`bg-black/80 w-full h-screen `}>
+        <div className={`flex flex-col justify-center h-screen`}>
+          <center>
+            <h1 className={`text-[30px]`}>Login</h1>
+          </center>
 
-                <div className={`flex flex-col justify-center h-screen`}>
-                    <center>
-                        <h1 className={`text-[30px]`}>Login</h1>
-                    </center>
+          <center>
+            <input
+              type='text'
+              placeholder='Enter Email Address'
+              className={`w-96  my-4 p-2 px-3 bg-transparent border-2 border-greenOverlay rounded-lg outline-none focus:border-ltPinkOverlay`}
+            />
+          </center>
 
-                    <center>
-                        <input type='text' placeholder='Enter Email Address' className={`w-96  my-4 p-2 px-3 bg-transparent border-2 border-greenOverlay rounded-lg outline-none focus:border-ltPinkOverlay`} />
-                    </center>
+          <center>
+            <input
+              type='password'
+              placeholder='Enter Password'
+              className={`w-96 my-4 p-2 px-3 bg-transparent border-2 border-greenOverlay rounded-lg outline-none focus:border-ltPinkOverlay`}
+            />
+          </center>
 
-                    <center>
-                        <input type='password' placeholder='Enter Password' className={`w-96 my-4 p-2 px-3 bg-transparent border-2 border-greenOverlay rounded-lg outline-none focus:border-ltPinkOverlay`} />
-                    </center>
+          <center>
+            <a href='#'>Forgot Password?</a>
+          </center>
 
-                    <center>
-                        <a href='#'>Forgot Password?</a>
-                    </center>
+          <center>
+            <a href='#'>Create An Account</a>
+          </center>
 
-                    <center>
-                        <a href='#'>Create An Account</a>
-                    </center>
-
-                    <center>
-                        <button type='submit'>Submit</button>
-                    </center>
-                </div>
-
-            </div>
-
+          <center>
+            <button type='submit'>Submit</button>
+          </center>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default EmailLogin
+export default EmailLogin;
--- frontend/src/container/Home.js
+++ frontend/src/container/Home.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import "../index.css";
 
 const Home = () => {
-    return (
-        <div>
-            Home
-        </div>
-    )
-}
+  return (
+    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
+      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]' />
+      <div className='p-5 text-white z-[2] mt-[-10rem]'>
+        <h1 className='text-9xl py-5'>OrchidFrenzy</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
