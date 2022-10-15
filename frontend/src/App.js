8308d8e8b1e6f3c1db5dd835e92927974e97bc7c
Ryan Howard <ryanghoward@protonmail.com>
2022-10-14 17:59:51 -0700

    Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import { Routes, Route, useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
-import Navbar from './components/navigation/navbar'
-import Login from './components/Login/Login'
-import EmailLogin from "./components/Login/EmailLogin"
-import Home from './container/Home'
+import Navbar from "./components/navigation/Navbar";
+import Login from "./components/Login/Login";
+import EmailLogin from "./components/Login/EmailLogin";
+import Home from "./container/Home";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
-  return(
-      <div>
-          <Navbar />
-          <Routes>
-            <Route path='/login' element={<Login />} />
-              <Route path='/emaillogin' element={<EmailLogin />} />
-            <Route path='/' element={<Home />} />
-          </Routes>
-      </div>
-  )
-}
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/emaillogin' element={<EmailLogin />} />
+        {/* <Route path='/' element={<Home />} /> */}
+      </Routes>
+    </div>
+  );
+};
 
 export default App;
--- frontend/src/container/Home.js
+++ frontend/src/container/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
+// import Navbar from "../components/navigation/Navbar";
 import "../index.css";
 
 const Home = () => {
   return (
-    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
-      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]' />
+    <div className='flex items-center justify-center h-screen mb-12'>
       <div className='p-5 text-white z-[2] mt-[-10rem]'>
-        <h1 className='text-9xl py-5'>OrchidFrenzy</h1>
+        <h1 className='block w-full py-2 text-transparent bg-clip-text leading-12 bg-gradient-to-r from-teal-400 to-purple-900 lg:inline text-9xl'>
+          OrchidFrenzy
+        </h1>
       </div>
     </div>
   );
