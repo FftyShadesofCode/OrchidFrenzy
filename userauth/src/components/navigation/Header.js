aa173cf16128d74eae1cbf830907e734552a27f0
Fifty Shades of Code <31264591+FftyShadesofCode@users.noreply.github.com>
2022-10-15 19:16:59 -0400

    Authorization is finished
    I still need to make it open the app

--- userauth/src/components/navigation/Header.js
+++ userauth/src/components/navigation/Header.js
@@ -10,7 +10,6 @@ import {
 import {
     FontAwesomeIcon
 } from '@fortawesome/react-fontawesome'
-import Logo from '../../assets/Logo.png'
 
 const Sidebar = () => {
     const [active, setActive] = useState(false)
@@ -18,17 +17,12 @@ const Sidebar = () => {
     const toggleSidebar = () => setActive(!active)
 
     return (
-        <div className={`transition-all duration-500 fixed m-4 rounded-lg shadow-md shadow-[0_10px_15px_10px_rgba(176,30,143,0.7)] border-r border-black border-md bg-primaryLtPink text-primaryCatt h-[96vh]`}>
+        <div className={`transition-all duration-500 fixed m-4 rounded-lg shadow-md shadow-[0_0_15px_10px_rgba(128,0,128,0.5)] border-r border-black border-md bg-teal text-white h-[96vh]`}>
             <div className={`pt-8 relative duration-300 ${active ? "w-72" : "w-20" }`}>
                 <div className={`flex gap-x-4 items-center ${active ? 'pr-12' : 'pr-0'}`}>
-                    <FontAwesomeIcon icon={faAnglesRight} className={`absolute cursor-pointer right-4 top-3 w-7 h-auto text-primaryCatt ${active && "rotate-180"} ${active ? 'right-4' : '-right-10'}`} onClick={toggleSidebar} />
-                    <img
-                        src={Logo}
-                        className={`cursor-pointer duration-500 w-[125px] h-auto ${active && "rotate-[360deg]" }`}
-                        alt=''
-                    />
-                    <h1 className={`text-primaryCatt origin-left font-medium text-3xl text-center duration-200 ${!active && "scale-0"} `}>
-                        <span className='text-secondaryGreen uppercase'>Orchi-D</span>ashboard
+                    <FontAwesomeIcon icon={faAnglesRight} className={`absolute cursor-pointer right-4 top-3 w-7 h-auto text-white ${active && "rotate-180"} ${active ? 'right-4' : '-right-10'}`} onClick={toggleSidebar} />
+                    <h1 className={`text-white origin-left font-medium text-3xl text-center duration-200 ${!active && "scale-0"} `}>
+                        <span className='text-transparent bg-clip-text bg-gradient-to-r from-teal to-primaryPurple text-5xl uppercase'>Orchi-D</span>ashboard
                     </h1>
                 </div>
                 <div className="relative m-2">
@@ -36,17 +30,17 @@ const Sidebar = () => {
                         className="absolute inset-y-0 left-0 flex items-center"
                         htmlFor="searchP"
                     >
-                        <FontAwesomeIcon icon={faSearch} className={`w-10 h-10 text-primaryCatt list-none h-[60px] no-underline hover:cursor-pointer duration-200 ${!active && 'ml-3'} ${active && 'text-primaryDeepPink scale-50'}`} />
+                        <FontAwesomeIcon icon={faSearch} className={`w-10 h-10 text-primaryPurple list-none h-[60px] no-underline hover:cursor-pointer duration-200 ${!active && 'ml-3'} ${active && 'text-sapphireBlue scale-50'}`} />
                     </label>
                     <input
                         id="searchP"
                         type="text"
                         placeholder="Search..."
-                        className={`w-full border border-gray-300 hover:border-primaryDeepPink pl-10 py-3 pr-4 text-primaryDeepPink rounded-md focus:outline-none focus:ring focus:border-mainGreen duration-200 placeholder-blackOverlay ${!active && "scale-0"}`}
+                        className={`w-full border border-gray-300 hover:border-primaryPurple pl-10 py-3 pr-4 text-indigo rounded-md focus:outline-none focus:ring focus:border-iris duration-200 placeholder-blackOverlay ${!active && "scale-0"}`}
                     />
                 </div>
 
-                <aside className='flex content-evenly text-primaryCatt' onClick={toggleSidebar}>
+                <aside className='flex content-evenly text-white' onClick={toggleSidebar}>
                     <nav>
                         {links.map((item, index) => {
                             return <SubHeader item={item} key={index} active={active} />;
--- userauth/src/components/navigation/SubHeader.js
+++ userauth/src/components/navigation/SubHeader.js
@@ -13,26 +13,26 @@ const SubHeader = ({ item, active, toggleSidebar }) => {
 
     return (
         <div>
-            <Link className='flex justify-between items-center text-primaryCatt text-xl p-5 list-none h-[60px] no-underline hover:bg-gradient-to-l from-secondaryGreen to-transparent hover:cursor-pointer mt-6' to={item.path}
+            <Link className='flex justify-between items-center text-white text-xl p-5 list-none h-[60px] no-underline hover:bg-gradient-to-l from-primaryPurple to-transparent hover:cursor-pointer mt-6' to={item.path}
                   onClick={item.subNav && showSubnav}>
                 <div className='flex items-center'>
-                    <FontAwesomeIcon icon={item.icon} className={`w-10 h-10 mr-6 text-secondaryGreen`} />
-                    <h1 className={`ml-[16px] text-primaryCatt origin-left text-2xl text-center mr-10 duration-200 ${!active && "hidden"}`} onClick={toggleSidebar}>{item.text}</h1>
+                    <FontAwesomeIcon icon={item.icon} className={`w-10 h-10 mr-6 text-white`} />
+                    <h1 className={`ml-[16px] text-white origin-left text-2xl text-center mr-10 duration-200 ${!active && "hidden"}`} onClick={toggleSidebar}>{item.text}</h1>
                 </div>
                 <div className={`w-5 h-5 duration-200 ${!active && "hidden"}`}>
                     {item.subNav && subnav
                         ? item.iconOpened
                         : item.subNav
-                            ? item.iconClosed
-                            : null}
+                        ? item.iconClosed
+                        : null}
                 </div>
             </Link>
             {subnav &&
                 item.subNav.map((item, index) => {
                     return (
-                        <Link to={item.path} key={index} className={`bg-deepPinkOverlay min-h-[60px] px-16 flex items-center no-underline text-primaryCatt text-xl hover:bg-greenOverlay opacity-1 duration-200`}>
+                        <Link to={item.path} key={index} className={`bg-irisOverlay min-h-[60px] px-16 flex items-center no-underline text-white text-xl hover:bg-gradient-to-l from-primaryPurple to-transparent opacity-1 duration-200`}>
                             <FontAwesomeIcon icon={item.icon} className={`w-5 h-5 duration-200 ${active && "opacity-1"}`} />
-                            <span className={`ml-[16px] text-primaryCatt origin-left font-medium text-2xl text-center duration-200 ${!subnav && "scale-0"}`}>{item.text}</span>
+                            <span className={`ml-[16px] text-white origin-left font-medium text-2xl text-center duration-200 ${!subnav && "scale-0"}`}>{item.text}</span>
                         </Link>
                     );
                 })}
