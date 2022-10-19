447b1a9c2f8dbe2c08ea0344197fa51851eab152
Fifty Shades of Code <31264591+FftyShadesofCode@users.noreply.github.com>
2022-10-19 14:18:47 -0400

    Initial Sidebar Push

--- frontend/src/components/navigation/sidenav.js
+++ frontend/src/components/navigation/sidenav.js
@@ -1,6 +1,115 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import useWindowDimensions from "../useWindowDimensions";
 
 export default function Sidenav() {
+    const { windowWidth } = useWindowDimensions()
+    const [selected, setSelected] = useState('home')
+    const [showArrows, setShowArrows] = useState(false)
+    const [translatedX, setTranslatedX] = useState(0)
+    const [translationWidth, setTranslationWidth] = useState(0)
+    const [isActive, setIsActive] = useState({ left: false, right: false })
+    const [hiddenItems, setHiddenItems] = useState({ left: 0, right: 0 })
+    const links = ['Home', 'Galleries', 'Friendz', 'Resources']
+
+    useEffect(() => {
+        // reset the translation on the X axis
+        setTranslatedX(0)
+
+        // handle active/show arrow conditions
+        if (windowWidth >= 690) {
+            setShowArrows(false)
+        }
+        else {
+            setShowArrows(true)
+            setIsActive({ left: false, right: true })
+        }
+
+        /**
+         * Different states depending on the width of the navbar
+         * */
+        if (windowWidth >= 665 && windowWidth < 690) {
+            setHiddenItems({ left: 0, right: 2 })
+            setTranslationWidth(129)
+        }
+        else if (windowWidth >= 590 && windowWidth < 665) {
+            setHiddenItems({ left: 0, right: 2 })
+            setTranslationWidth(107)
+        }
+        else if (windowWidth >= 500 && windowWidth < 590) {
+            setHiddenItems({ left: 0, right: 3 })
+            setTranslationWidth(121)
+        }
+        else if (windowWidth >= 432 && windowWidth < 500) {
+            setHiddenItems({ left: 0, right: 3 })
+            setTranslationWidth(94)
+        }
+        else if (windowWidth >= 410 && windowWidth < 430) {
+            setTranslationWidth(130)
+            setHiddenItems({ left: 0, right: 4 })
+        }
+        else if (windowWidth >= 370 && windowWidth < 410) {
+            setTranslationWidth(125)
+            setHiddenItems({ left: 0, right: 3 })
+        }
+        else if (windowWidth >= 350 && windowWidth < 370) {
+            setHiddenItems({ left: 0, right: 4 })
+            setTranslationWidth(118)
+        }
+        else if (windowWidth >= 320 && windowWidth < 350) {
+            setHiddenItems({ left: 0, right: 4 })
+            setTranslationWidth(105)
+        }
+        else if (windowWidth >= 300 && windowWidth < 320) {
+            setHiddenItems({ left: 0, right: 5 })
+            setTranslationWidth(190)
+        }
+        else if (windowWidth >= 275 && windowWidth < 300) {
+            setHiddenItems({ left: 0, right: 5 })
+            setTranslationWidth(159)
+        }
+        else if (windowWidth >= 260 && windowWidth < 275) {
+            setHiddenItems({ left: 0, right: 5 })
+            setTranslationWidth(145)
+        }
+        else if (windowWidth >= 249 && windowWidth < 260) {
+            setHiddenItems({ left: 0, right: 5 })
+            setTranslationWidth(130)
+        }
+        else if (windowWidth < 249) {
+            setHiddenItems({ left: 0, right: 5 })
+            setTranslationWidth(95)
+        }
+    }, [windowWidth])
+
+    function moveRight() {
+        if (!isActive.left) setIsActive({ ...isActive, left: true })
+
+        if (hiddenItems.right > 1) {
+            setHiddenItems({ left: hiddenItems.left + 1, right: hiddenItems.right - 1 })
+            setTranslatedX(translatedX + translationWidth)
+        }
+        else if (hiddenItems.right === 1) {
+            setHiddenItems({ left: hiddenItems.left + 1, right: 0 })
+            setTranslatedX(translatedX + translationWidth)
+            setIsActive({ right: false, left: true })
+        }
+    }
+
+    function moveLeft() {
+        if (!isActive.right) setIsActive({ ...isActive, right: true })
+
+        if (hiddenItems.left > 1) {
+            setHiddenItems({ left: hiddenItems.left - 1, right: hiddenItems.right + 1 })
+            setTranslatedX(translatedX - translationWidth)
+        }
+        else if (hiddenItems.left === 1) {
+            setHiddenItems({ left: 0, right: hiddenItems.right + 1 })
+            setTranslatedX(translatedX - translationWidth)
+            setIsActive({ right: true, left: false })
+        }
+    }
+
+
     return (
         <div>
 
