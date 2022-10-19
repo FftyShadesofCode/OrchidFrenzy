ebb66d3cdc63297fad5b69025027698c0c63c841
Ryan Howard <ryanghoward@protonmail.com>
2022-10-19 10:55:56 -0700

    New Register Page/Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Topnav from "./components/navigation/topnav";
 import Login from "./components/Login/Login";
-// import Register from './components/Login/Register'
+import Register from "./components/Login/Register";
 import Home from "./pages/Home";
 
 import "./App.css";
@@ -15,9 +15,10 @@ const App = () => {
       <Routes>
         <Route element={<Home />} path='/' exact />
         <Route path='/login' element={<Login />} />
-        {/* <Route element={<Register />} path='/register' /> */}
+        <Route element={<Register />} path='/register' />
       </Routes>
     </div>
   );
 };
 
+export default App;
