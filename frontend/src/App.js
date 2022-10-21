627b4ce8da262361f18958e9151d486ae912bdbd
Fifty Shades of Code <31264591+FftyShadesofCode@users.noreply.github.com>
2022-10-21 10:04:00 -0400

    Fixed routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,13 @@ import Sidenav from "./components/navigation/sidenav";
 import Login from "./components/Login/Login";
 import Register from "./components/Login/Register";
 import Home from "./pages/Home";
+import Messaging from './pages/Messaging/messaging';
+import Inbox from './pages/Messaging/Inbox';
+import Favorites from "./pages/Messaging/Favorites";
+import Sent from './pages/Messaging/Sent';
+import Friendz from './pages/Friendz/friendz';
+import MyOrchids from './pages/MyOrchids/MyOrchids';
+import Forums from './pages/Forums/forums';
 
 import "./App.css";
 
@@ -18,6 +25,15 @@ const App = () => {
         <Route element={<Home />} path='/' exact />
         <Route path='/login' element={<Login />} />
         <Route element={<Register />} path='/register' />
+
+        // Sidebar Routes
+          <Route path='/messaging' element={<Messaging />} />
+          <Route path='/messaging/inbox' element={<Inbox />} />
+          <Route path='/messaging/sent' element={<Sent />} />
+          <Route path='/messaging/favorites' element={<Favorites />} />
+          <Route path='/friendz' element={<Friendz />} />
+          <Route path='/my-orchids' element={<MyOrchids />} />
+          <Route path='/forums' element={<Forums />} />
       </Routes>
     </div>
   );
