f281f712c0bd75842ff1f51b1095334f4abd16bd
Fifty Shades of Code <31264591+FftyShadesofCode@users.noreply.github.com>
2022-10-20 22:53:17 -0400

    Sidebar is done

--- frontend/src/components/Login/Register.js
+++ frontend/src/components/Login/Register.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
 import { UserAuth } from "../../utils/AuthContext";
 import { signInWithGoogle, signInWithFacebook } from "../../utils/firebase";
-
 import { FcGoogle } from "react-icons/fc";
 import { AiFillFacebook } from "react-icons/ai";
-
 import Account from "../accounts/account";
+import States from "./StatesArray";
+import Button from "../Button/button";
 
 const Register = () => {
   // Email Sign-In
@@ -31,225 +30,124 @@ const Register = () => {
   // Google Sign-In
 
   return (
-    <div className='bg-cTenebrosa bg-cover bg-center w-screen h-screen text-white'>
-      <div
-        className={`bg-black/50 w-full h-screen flex items-center justify-center`}
-      >
-        <form className='w-full max-w-lg' onSubmit={handleSubmit}>
-          <div className='flex flex-wrap -mx-3 mb-6'>
-            <div className='w-full md:w-1/2 px-3 mb-6 md:mb-0'>
-              <label
-                className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                htmlFor='grid-first-name'
-              >
-                First Name
-              </label>
-              <input
-                className='appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
-                id='grid-first-name'
-                type='text'
-                placeholder='First'
-              />
-              {/* <p className='text-red-500 text-xs italic'>
-                  Please fill out this field.
-                </p> */}
-            </div>
-            <div className='w-full md:w-1/2 px-3'>
-              <label
-                className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                htmlFor='grid-last-name'
-              >
-                Last Name
-              </label>
-              <input
-                className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                id='grid-last-name'
-                type='text'
-                placeholder='Last'
+      <div className='flex items-center h-full bg-white mt-10'>
+        <div className='flex-1h-full max-w-4xl mx-auto mb-12 bg-purple-100 rounded-xl shadow-2xl'>
+          <div className='flex flex-col md:flex-row'>
+            <div className='h-32 md:h-auto md:w-1/2'>
+              <img
+                  className='object-cover w-full h-full'
+                  src='https://images.unsplash.com/photo-1605996370592-b6f7a81e382e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8b3JjaGlkfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
+                  alt='img'
               />
             </div>
-          </div>
-          <div className='flex flex-wrap -mx-3 mb-6'>
-            <div className='w-full px-3'>
-              <label
-                className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                htmlFor='grid-password'
-              >
-                Email
-              </label>
-              <input
-                className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                id='grid-email'
-                type='email'
-                placeholder='Email'
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className='flex flex-wrap -mx-3 mb-6'>
-            <div className='w-full px-3'>
-              <label
-                className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                htmlFor='grid-password'
-              >
-                Password
-              </label>
-              <input
-                className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                id='grid-password'
-                type='password'
-                placeholder='************'
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <p className='text-gray-600 text-xs italic'>
-                Make it as long and as crazy as you'd like
-              </p>
-            </div>
-          </div>
-          <div className='flex flex-wrap -mx-3 mb-2'>
-            <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
-              <label
-                className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                htmlFor='grid-city'
-              >
-                City
-              </label>
-              <input
-                className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                id='grid-city'
-                type='text'
-                placeholder='City'
-              />
-            </div>
-            <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
-              <label
-                className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                htmlFor='grid-state'
-              >
-                State
-              </label>
-              <div className='relative'>
-                <select
-                  className='block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                  id='grid-state'
-                >
-                  <option>Alabama</option>
-                  <option>Alaska</option>
-                  <option>Arizona</option>
-                  <option>Arkansas</option>
-                  <option>California</option>
-                  <option>Colorado</option>
-                  <option>Connecticut</option>
-                  <option>Delaware</option>
-                  <option>Florida</option>
-                  <option>Georgia</option>
-                  <option>Hawaii</option>
-                  <option>Idaho</option>
-                  <option>Illinois</option>
-                  <option>Indiana</option>
-                  <option>Iowa</option>
-                  <option>Kansas</option>
-                  <option>Kentucky</option>
-                  <option>Louisiana</option>
-                  <option>Maine</option>
-                  <option>Maryland</option>
-                  <option>Massachusetts</option>
-                  <option>Michigan</option>
-                  <option>Minnesota</option>
-                  <option>Mississippi</option>
-                  <option>Missouri</option>
-                  <option>Montana</option>
-                  <option>Nebraska</option>
-                  <option>Nevada</option>
-                  <option>New Hampshire</option>
-                  <option>New Jersey</option>
-                  <option>New Mexico</option>
-                  <option>New York</option>
-                  <option>North Carolina</option>
-                  <option>North Dakota</option>
-                  <option>Ohio</option>
-                  <option>Oklahoma</option>
-                  <option>Oregon</option>
-                  <option>Pennsylvania</option>
-                  <option>Rhode Island</option>
-                  <option>South Carolina</option>
-                  <option>South Dakota</option>
-                  <option>Tennessee</option>
-                  <option>Texas</option>
-                  <option>Utah</option>
-                  <option>Vermont</option>
-                  <option>Virginia</option>
-                  <option>Washington</option>
-                  <option>West Virginia</option>
-                  <option>Wisconsin</option>
-                  <option>Wyoming</option>
-                </select>
-                <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
-                  <svg
-                    className='fill-current h-4 w-4'
-                    xmlns='http://www.w3.org/2000/svg'
-                    viewBox='0 0 20 20'
-                  >
-                    <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
-                  </svg>
+            <div className='flex items-center justify-center p-6 sm:p-12 md:w-1/2'>
+              <form className='w-full h-full' onSubmit={handleSubmit}>
+                <h1 className='mb-8 text-2xl font-bold text-center text-purple-700'>
+                  Create an Account
+                </h1>
+                <input
+                    type='email'
+                    className='w-full mb-8 px-4 py-2 h-[2.5rem] text-sm border rounded-md focus:border-purple-500 focus:outline-none focus:ring-1 focus:ring-purple-900'
+                    placeholder='Email'
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    className='w-full px-4 py-2 h-[2.5rem] text-sm border rounded-md focus:border-purple-500 focus:outline-none focus:ring-1 focus:ring-purple-900'
+                    placeholder='Password'
+                    type='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    className='w-full px-4 py-2 h-[2.5rem] mt-4 text-sm border rounded-md focus:border-purple-500 focus:outline-none focus:ring-1 focus:ring-purple-900'
+                    placeholder='Confirm Password'
+                    type='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <div className='flex flex-wrap -mx-3 mb-2 mt-10'>
+                  <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
+                    <input
+                        className='w-full px-4 py-2 h-[2.5rem] text-sm border rounded-md focus:border-purple-500 focus:outline-none focus:ring-1 focus:ring-purple-900'
+                        id='grid-city'
+                        type='text'
+                        placeholder='City'
+                    />
+                  </div>
+                  <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
+                    <div className='relative'>
+                      <select
+                          className='block appearance-none w-full px-4 py-2 h-[2.5rem] text-sm border rounded-md focus:border-purple-500 focus:outline-none focus:ring-1 focus:ring-purple-900'
+                          id='grid-state'
+                      >
+                        {States.map((state) => {
+                          return (
+                              <option key={state.id} value={state.name}>
+                                {state.name}
+                              </option>
+                          );
+                        })}
+                      </select>
+                      <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700'>
+                        <svg
+                            className='fill-current h-4 w-4'
+                            xmlns='http://www.w3.org/2000/svg'
+                            viewBox='0 0 20 20'
+                        >
+                          <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
+                    <input
+                        className='w-full px-4 py-2 h-[2.5rem] text-sm border rounded-md focus:border-purple-500 focus:outline-none focus:ring-1 focus:ring-purple-9000'
+                        id='grid-zip'
+                        type='text'
+                        placeholder='Zip'
+                    />
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className='w-full md:w-1/3 px-3 mb-6 md:mb-0'>
-              <label
-                className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-                htmlFor='grid-zip'
-              >
-                Zip
-              </label>
-              <input
-                className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
-                id='grid-zip'
-                type='text'
-                placeholder='Zip'
-              />
+                <Button
+                    text='Create Account'
+                    type='button'
+                    buttonStyle='square'
+                    handleClick={() => console.log("Clicked!")}
+                />
+                <div
+                    style={{
+                      borderTop: "1px solid #301934",
+                      marginTop: 30,
+                    }}
+                ></div>
+                <div className='flex flex-col justify-center gap-4 mt-8'>
+                  <button
+                      onClick={signInWithGoogle}
+                      className='p-4 w-full font-medium rounded-lg flex justify-center gap-2 border-2 border-purple-900 hover:bg-white hover:border-purple-500'
+                  >
+                    <FcGoogle className='text-2xl text-purple-900' />
+                    Sign Up with Google
+                  </button>
+                  <button
+                      onClick={signInWithFacebook}
+                      className='p-4 w-full font-medium rounded-lg flex justify-center gap-2 border-2 border-purple-900 hover:bg-white hover:border-purple-500'
+                  >
+                    <AiFillFacebook className='text-facebook text-2xl' />
+                    Sign Up with Facebook
+                  </button>
+                  <p className='py-2 text-sm text-purple-900 flex justify-center'>
+                    Already have an account?
+                    <Link
+                        to='/login'
+                        className='mx-2 text-purple-900 text-sm hover:underline duration-300'
+                    >
+                      Login
+                    </Link>
+                  </p>
+                </div>
+              </form>
             </div>
           </div>
-          <div className={`flex flex-col justify-center gap-4 mt-10`}>
-            <button
-              onClick={signInWithGoogle}
-              className={`bg-gray-500 p-4 w-full font-medium rounded-lg flex align-middle gap-2 border-2 border-gray-700 shadow-inner shadow-md shadow-gray-700 hover:bg-gray-900 hover:border-gray-400 hover:shadow-md hover:shadow-gray-200 hover:shadow-inner`}
-            >
-              <FcGoogle className={`text-2xl`} />
-              Sign up with Google
-            </button>
-
-            <button
-              onClick={signInWithFacebook}
-              className={`bg-gray-500 p-4 w-full font-medium rounded-lg flex align-middle gap-2 border-2 border-gray-700 shadow-inner shadow-md shadow-gray-700 hover:bg-gray-900 hover:border-gray-400 hover:shadow-md hover:shadow-gray-200 hover:shadow-inner`}
-            >
-              <AiFillFacebook className={`text-facebook text-2xl`} />
-              Sign up with Facebook
-            </button>
-          </div>
-          <center className={`my-6`}>
-            <button
-              type='submit'
-              className={`p-4 w-full text-white bg-gray-700 font-medium rounded-lg flex justify-center align-center gap-2 hover:bg-gray-500`}
-            >
-              Submit
-            </button>
-          </center>
-          <center className={`no-underline`}>
-            <p className={`py-2 text-gray-700`}>
-              Already have an account?
-              <Link
-                to='/login'
-                className={`mx-4 text-gray-700 text-lg hover:underline duration-300`}
-              >
-                Sign In
-              </Link>
-            </p>
-          </center>
-        </form>
+        </div>
       </div>
-    </div>
   );
 };
 
