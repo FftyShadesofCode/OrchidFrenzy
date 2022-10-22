bf5e9fdca5ce7da2f820f1433b148db3978a6e22
Fifty Shades of Code <31264591+FftyShadesofCode@users.noreply.github.com>
2022-10-22 14:43:27 -0400

    Fixing UI

--- frontend/src/components/navigation/sidenav.js
+++ frontend/src/components/navigation/sidenav.js
@@ -21,7 +21,7 @@ const Sidenav = () => {
     }
 
     return (
-        <div className={`transition-all duration-500 fixed right-0 mr-[25px] mb-[25px] mt-16 rounded-2xl border-2 border-primaryPurple border-md bg-gradient-to-tl from-darkPurple to-teal text-white h-[80vh] ${showSidebar ? 'w-72' : 'w-20'}`}>
+        <div className={`transition-all duration-500 fixed right-0 mr-[25px] mb-[25px] top-28 pb-10 rounded-2xl border-2 border-primaryPurple border-md bg-gradient-to-tl from-darkPurple to-teal text-white h-[80vh] ${showSidebar ? 'w-72' : 'w-20'}`}>
             <button className={`flex gap-x-4 w-10 h-10 items-center`} onClick={toggleSidebar} >
                 <FontAwesomeIcon icon={faAnglesLeft} className={`absolute cursor-pointer right-4 top-3 w-7 h-auto text-white duration-500 ${showSidebar && 'rotate-180'}`} />
             </button>
